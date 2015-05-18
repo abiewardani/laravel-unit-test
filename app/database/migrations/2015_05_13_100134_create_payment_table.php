55c1baa3ad5c7fd4b255b2e1ab04e859d1421562
abiewardani <abiwardani1990@gmail.com>
2015-05-18 14:53:10 +0700

    update unit test

---
+++ app/database/migrations/2015_05_13_100134_create_payment_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePaymentTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('payment', function(Blueprint $table)
+		{
+			$table->increments('id');
+			 $table->string('ip');
+            $table->integer('hits');
+            $table->string('online');
+            $table->string('url');
+            $table->string('path');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('payment');
+	}
+
+}
